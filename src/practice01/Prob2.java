594a19b33482f25cfb035ec4066cd5cff8fdff70
Alex <urimaumdero@hotmail.com>
2017-08-01 15:59:26 +0900

    .

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -3,7 +3,7 @@ package practice01;
 public class Prob2 {
 	public static void main(String[] args) {
 		
-		for(int i=0; i<10; i++){
+		for(int i=0; i<9; i++){
 			for(int j=0; j<10; j++){
 				
 				System.out.print(i+j+1+" ");
--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -4,17 +4,18 @@ public class Prob5 {
 	public static void main(String[] args) {
 		
 		String st;
-		for(int i=1; i<1000; i++){
+		for(int i=1; i<10000; i++){
 			
 			st = Integer.toString(i);
 			//st = String.valueOf(i);
 			
 			if(st.contains("3") || st.contains("6") || st.contains("9")){
 				
-				int clapNum=0;
+				int clapNum = 0;
+				String separateNum = null;
 				for(int j=0; j<st.length(); j++){
 
-					String separateNum = st.substring(j, j+1);
+					separateNum = st.substring(j, j+1);
 					if(separateNum.equals("3") || separateNum.equals("6") || separateNum.equals("9")){
 						clapNum++;
 					}
